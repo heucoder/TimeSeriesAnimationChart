840307a91375c902a6131cae7848826e500732d0
yant <812860165@qq.com>
2019-09-27 16:19:38 +0800

    version 0.1.0 add cli

--- AnimationChart.py
+++ AnimationChart.py
@@ -1,21 +1,14 @@
 #coding=utf-8
 
 # plot animation
-import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import matplotlib.animation as animation
-import pandas as pd
-
 from matplotlib.font_manager import FontProperties
+import pandas as pd
 from functools import cmp_to_key
-
-# unused
-from IPython.display import HTML
-
-# logging
 import logging
-
+from docopt import docopt
 # 
 from config import AllColors, MARKERS, FONT_PATH
 from utils import FileType2
@@ -28,21 +21,17 @@ font2 = FontProperties(fname=FONT_PATH, size = 12, weight=600)
 core class AnimationBarChart
 '''
 
+class AnimationChart():
 
-class AnimationBarChart():
-
-    def __init__(self, datasets, val, key, name, contents = "", selected = None):
+    def __init__(self, datasets, val, key, name, contents = ""):
         self._datasets = datasets
         self._val = val
         self._key = key
         self._name = name
         self._contents = contents
-        self._selected = selected
         self._colors = None
         self._fig = None
         self._ax = None
-        self._animator = None
-        self._title = None
     
     def _loadcolors(self):
         # need to update
@@ -89,7 +78,7 @@ class AnimationBarChart():
 
     def _drawBarChart(self, k):
         # need to update
-        dff = self._datasets[self._datasets[self._key].eq(k)].sort_values(by=self._val, ascending=True).tail(self._display_num)
+        dff = self._datasets[self._datasets[self._key].eq(k)].sort_values(by=self._val, ascending=True).tail(self._bar_display_num)
         self._ax.clear()
         self._ax.barh(dff[self._name], dff[self._val], color = [self._colors[x] for x in dff[self._name]])
 
@@ -115,8 +104,9 @@ class AnimationBarChart():
     def _drawPieChart(self, k):
         # 分为selected和未selected两部分
         dff = self._datasets[self._datasets[self._key].eq(k)]
-        otherdff = self._otherdf[self._otherdf[self._key].eq(k)]
-        dff = pd.concat([dff, otherdff], axis = 0)
+        if self._otherdf != None:
+            otherdff = self._otherdf[self._otherdf[self._key].eq(k)]
+            dff = pd.concat([dff, otherdff], axis = 0)
 
         self._ax.clear()
         self._ax.pie(x = dff[self._val], labels = dff[self._name], autopct='%1.2f%%', center=(8,6), radius = 1, labeldistance = 1.1,
@@ -132,14 +122,14 @@ class AnimationBarChart():
         self._ax.clear()
         for name in df[self._name]:
             item = dff[dff[self._name].eq(name)]
-            self._ax.plot(item[self._key], item[self._val],c = self._colors[name], marker = self._markers[name], lw = 2, ms = 5, label = name)
+            self._ax.plot(item[self._key], item[self._val],c = self._colors[name], marker = self._markers[name], lw = 1.5, ms = 3, label = name)
         
         dx = 0.03
         for _, (value, key, name) in enumerate(zip(df[self._val], df[self._key], df[self._name])):
             self._ax.text(key, value*(1+dx), name, size = 10, weight=400, va='bottom', color = '#444444', fontproperties=font1)
             self._ax.text(key, value*(1+dx), f'{value:,.0f}', size = 12, weight=400, va='top', color = '#444433')           
 
-        self._ax.grid(which = 'major', axis = 'y', color='c', linestyle='--', linewidth=1, alpha=0.3)  
+        self._ax.grid(which = 'major', axis = 'y', linestyle='--', linewidth=1, alpha=0.3)  
         self._ax.set_xlabel('{}'.format(self._key), size = 20, color = '#777777', weight = 400)
         self._ax.yaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
         self._ax.set_ylabel('{}'.format(self._val), size = 20, color = '#777777', weight = 400)
@@ -148,27 +138,29 @@ class AnimationBarChart():
         self._ax.legend(prop=font2) #
         plt.title('{}'.format(self._title), fontproperties = font_title, color = '#777777')
         
-    def animation(self, chart_type = 'line', display_num = 10, interval = 200, title = '', repeat = False, frames = None, cmp=None, reverse = False, saveflag = True):
-        #need to update
-        self._display_num = display_num
+    def animation(self, chart_type = 'line', bar_display_num = 10, selected=None,title = '', saveflag = False,
+                 interval = 200, repeat = False, frames = None,
+                 cmp=None, reverse = False, ):
+
         # check
         if self._checkdataset():
             logging.error("file check error")
             return 
         
-        # load_colors, maekers
+        # loads 
         self._colors = self._loadcolors()
         self._markers = self._loadmarkers()
+        self._selected = selected
         self._title = title
-        
-        #make frame
+        self._bar_display_num = bar_display_num
+
+        # make frame
         if(frames == None):
             frames = self._make_frame(cmp, reverse)
-        self._frames = frames
         self._frameMax = frames[-1]
         self._frameMin = frames[0]
 
-        # select chart_type
+        # chart_type
         drawFunc = None
         if chart_type == 'bar':
             drawFunc = self._drawBarChart
@@ -181,7 +173,8 @@ class AnimationBarChart():
             logging.error("select right draw funcs")
             return 
         
-        #
+        # 
+        self._otherdf = None
         if self._selected != None:
             df_tmp = self._datasets[~self._datasets[self._name].isin(self._selected)]
             df = df_tmp[self._val].groupby(df_tmp[self._key]).sum()
@@ -195,35 +188,42 @@ class AnimationBarChart():
 
         # make animator 
         self._fig, self._ax = plt.subplots(figsize = (15, 8))
-        plt.title('{}'.format('各国人口数量'), color = '#444444', fontproperties=font1)
         animator = animation.FuncAnimation(self._fig, drawFunc, frames=frames, interval = interval ,repeat = repeat)
         if saveflag:
             animator.save('{}-{}.gif'.format(self._title, chart_type), writer='imagemagick')
-
         plt.show()
 
+def cli():
+    cli_docs = """
+    TEST    
+
+    Usage:
+        AnimationChart.py (bar | line | pie) <file_path> <val> <key> <name> [--contents=<contents>] [--bar_display_num=<bar_display_num>] [--selected=<selectd>] [--title=<title>]
+        AnimationChart.py (-h | --help)
+    
+    Options:
+        -h --help     Show this screen.
+
+    """
+    arguments = docopt(cli_docs)
+    file_path = arguments['<file_path>']
+    val = arguments['<val>']
+    key = arguments['<key>']
+    name = arguments['<name>']
+    contents = arguments['--contents'] if arguments['--contents'] else ""
+    if arguments['bar'] == True:
+        chart_type='bar'
+    elif arguments['line'] == True:
+        chart_type = 'line'
+    elif arguments['pie'] == True:
+        chart_type = 'pie'
+    bar_display_num = 10 if not arguments['--bar_display_num'] else int(arguments['--bar_display_num'])
+    selectd = arguments['--selected'].split(' ') if arguments['--selected'] else None
+    title  = arguments['--title'] if arguments['--title'] else ""
+    print(arguments)
+    datasets = FileType2.load_file(file_path)
+    a = AnimationChart(datasets, val, key, name, contents)
+    a.animation(chart_type, bar_display_num, selectd, title)
+
 if __name__ == "__main__":
-    # datasets = load_datasets()
-    path = 'population_data.csv'
-    datasets = FileType2.load_file(path)
-    selected = ['中国', '美国', '印度']
-    b = AnimationBarChart(datasets, 'val', 'year', 'country', 'states', selected=selected)
-    b.animation(chart_type = 'line', display_num=5, title = '各国人口数目')
-
-
-    # datasets['val'] = datasets['val'].apply(lambda x: eval(x.replace(',','')))
-    # df = datasets['val'].groupby(datasets['year']).sum()
-    # print(df.index)
-    # dff = pd.DataFrame()
-    # datasets['year'] = df.index
-    # datasets['val'] = df.values
-    # datasets['country'] = 'others'
-    # datasets['states'] = 'others'
-    # print(datasets.head())
-
-
-
-    # print(type(datasets['val'][0]))
-    # datasets['val'] = datasets['val'].apply(lambda x: eval(x.replace(',','')))
-    # print(datasets['val'])
-    # print(isinstance(int, object))
+    cli()
--- example/example1.py
+++ example/example1.py
@@ -2,22 +2,22 @@
 import sys
 sys.path.append('..')
 
-from DynamicSortVis import AnimationBarChart
+from AnimationChart import AnimationChart
 from utils import FileType2
 
 def example_bar():
-    b = AnimationBarChart(datasets, 'val', 'year', 'country', 'states')
+    b = AnimationChart(datasets, 'val', 'year', 'country', 'states')
     b.animation(chart_type = 'bar', title = '各国人口数目')
 
 def example_line():
     selected = ['中国', '美国', '印度']
-    b = AnimationBarChart(datasets, 'val', 'year', 'country', 'states', selected=selected)
-    b.animation(chart_type = 'line', title = '各国人口数目')
+    b = AnimationChart(datasets, 'val', 'year', 'country', 'states',)
+    b.animation(chart_type = 'line', title = '各国人口数目', selected=selected)
 
 def example_pie():
     selected = ['中国', '美国', '印度']
-    b = AnimationBarChart(datasets, 'val', 'year', 'country', 'states', selected=selected)
-    b.animation(chart_type = 'pie', title = '各国人口比例')
+    b = AnimationChart(datasets, 'val', 'year', 'country', 'states')
+    b.animation(chart_type = 'pie', title = '各国人口比例', selected=selected)
 
 if __name__ == "__main__":
     path = 'population_data.csv'
